a66db408f027038551ccadc0834c47f224e319df
Muraleekrishna Gopinathan <muraleekrishnagc@gmail.com>
2019-08-15 17:47:06 +1000

    Add CNN

---
+++ cnn.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5)  # torch.Size([1, 32, 24, 24])
+        self.relu1 = nn.ReLU()  # torch.Size([1, 32, 24, 24])
+        self.batch_norm1 =  nn.BatchNorm2d(num_features=32)  # torch.Size([1, 32, 24, 24])
+        self.pool1 = nn.MaxPool2d(stride=2, kernel_size=(2, 2))  # torch.Size([1, 64, 12, 12])
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3), padding=1, stride=1)  # torch.Size([1, 64, 12, 12])
+        self.relu2 = nn.ReLU()  # torch.Size([1, 64, 12, 12])
+        self.batch_norm2 = nn.BatchNorm2d(num_features=64)  # torch.Size([1, 64, 12, 12])
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)  # torch.Size([1, 64, 6, 6])
+        # nn.Dropout(),
+        self.fc1 =  nn.Linear(64*6*6, 1024)
+        self.relu3 = nn.ReLU()
+        self.fc2 = nn.Linear(in_features=1024, out_features=10)
+
+    def forward(self, x):
+        x = self.relu1(self.conv1(x))
+        x = self.pool1(self.batch_norm1(x))
+        x = self.relu2(self.conv2(x))
+        x = self.pool2(self.batch_norm2(x))
+        x = x.view(-1, 64*6*6)
+        x = self.relu3(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+# 5×5 Convolutional Layer with 32 filters, stride 1 and padding 1.
+# • ReLU Activation Layer.
+# • Batch Normalization Layer
+# • 2×2 Max Pooling Layer with a stride of 2.
+# • 3×3 Convolutional Layer with 64 filters, stride 1 and padding 1.
+# • ReLU Activation Layer.
+# • Batch Normalization Layer.
+# • 2×2 Max Pooling Layer with a stride of 2.
+# • Fully-connected layer with 1024 output units.
+# • ReLU Activation Layer.
+# • Fully-connected layer with 10 output units.
---
+++ mnist_pytorch.py
@@ -0,0 +1,60 @@
+import torch
+import torchvision
+from torch import nn, optim  # Contains several Pytorch optimizer classes
+from torchvision import transforms
+from torch.utils.data import *
+
+import cnn
+
+
+transform = transforms.Compose(
+    [
+        transforms.RandomCrop(28, padding=4, pad_if_needed=False, fill=0, padding_mode='constant'),
+        transforms.Grayscale(num_output_channels=1),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize([0], [1])
+    ]
+)
+
+# train_idx, valid_idx, testidx = list(range(num_training)), list(range(num_validation)), list(range(num_test))
+#
+# train_loader = DataLoader(mnist_dataset, batch_size=50,sampler=SubsetRandomSampler(train_idx), num_workers=0)
+# val_loader = DataLoader(mnist_dataset, batch_size=50,sampler=SubsetRandomSampler(valid_idx), num_workers=0)
+# test_loader = DataLoader(mnist_dataset, batch_size=50,sampler=SubsetRandomSampler(valid_idx), num_workers=0)
+
+train_set = torchvision.datasets.FashionMNIST(root='./', train=True, download=False, transform=transform)
+train_loader = DataLoader(train_set, batch_size=4, shuffle=True, num_workers=2)
+
+val_set = torchvision.datasets.FashionMNIST(root='./', train=False, download=False, transform=transform)
+val_loader = DataLoader(val_set, batch_size=4, shuffle=False, num_workers=2)
+
+net = cnn.CNN()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# output = net(torch.rand(1, 1, 28, 28))
+# print(output.shape)
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
