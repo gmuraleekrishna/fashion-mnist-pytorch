ccadcb011ec46944598c7f858feb4f9da3e4a968
Muraleekrishna Gopinathan <muraleekrishnagc@gmail.com>
2019-08-22 17:09:15 +1000

    Change dataset to fashion MNIST

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,6 @@
 import torchvision
 from torchvision import transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
 import numpy as np
 
 train_transform = transforms.Compose(
@@ -26,9 +26,9 @@ transform = transforms.Compose([
 
 
 def load_data(batch_size, valid_size=0.02):
-	train_dataset = torchvision.datasets.MNIST(root='./', train=True, download=True, transform=train_transform)
-	val_dataset = torchvision.datasets.MNIST(root='./', train=True, download=False, transform=val_transform)
-	test_dataset = torchvision.datasets.MNIST(root='./', train=True, download=True, transform=transform)
+	train_dataset = torchvision.datasets.FashionMNIST(root='./', train=True, download=True, transform=train_transform)
+	val_dataset = torchvision.datasets.FashionMNIST(root='./', train=True, download=False, transform=val_transform)
+	test_dataset = torchvision.datasets.FashionMNIST(root='./', train=True, download=True, transform=transform)
 
 	num_train = len(train_dataset)
 	indices = list(range(num_train))
@@ -36,7 +36,8 @@ def load_data(batch_size, valid_size=0.02):
 
 	train_indices, val_indices = indices[split:], indices[:split]
 
-	train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=2, sampler=SubsetRandomSampler(train_indices))
+	train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=2,
+	                          sampler=SubsetRandomSampler(train_indices))
 	val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=2, sampler=SubsetRandomSampler(val_indices))
 	test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=2, shuffle=True)
 	return train_loader, val_loader, test_loader
